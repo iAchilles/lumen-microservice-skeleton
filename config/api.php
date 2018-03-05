5849f2fcce0dbbc31f2f4e85b9fa6d5dec3f8212
Igor Manturov Jr <igor.manturov.jr@gmail.com>
2018-03-04 23:45:44 -0300

    Added config files

---
+++ config/api.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'name'     => env('NODE_NAME', 'node1'),
+    'protocol' => env('NODE_PROTOCOL', 'http'),
+    'host'     => env('NODE_HOST', 'localhost'),
+    'secret'   => env('NODE_SECRET', '000100101100101101'),
+    'ip'       => null,
+];
